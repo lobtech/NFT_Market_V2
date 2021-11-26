a2ea2b2a38fd1726e5c05af800f297f12eb5763e
Breathe <15983966644@qq.com>
2021-11-26 20:32:32 +0800

    sd

--- src/store/modules/moralis_module.ts
+++ src/store/modules/moralis_module.ts
@@ -8,7 +8,8 @@ const states = {
     serverUrl: 'https://iryujqeq3c0k.usemoralis.com:2053/server' as string, // 服务器地址
     appId: 'pgxFvaT0RRCnC9efXnb6f6Ck9WxudIoscdigwwpU' as string, // appid
     chain: 'bsc testnet' as string, // 区块链名
-    address: '0xffec052ee58e1a4d5848a61e7a68a79a94cd5c0e' as string, // 合约地址
+    address: '0xF55c6Be2F9390301bFc66Dd9f7f52495B56301dC' as string, // 合约地址
+    // address: '0xffec052ee58e1a4d5848a61e7a68a79a94cd5c0e' as string, // 合约地址
     user: {
         accounts: '' as string, // 用户地址
         username: '' as string, // 用户昵称
